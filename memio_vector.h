6494ede0f31261adf3f2cb7cd98292e70325c8bb
Fred Brooks <nsaspook@nsaspook.com>
2015-10-23 23:11:39 -0700

    software coding,

---
+++ memio_vector.h
@@ -0,0 +1,104 @@
+/* 
+ * File:   memio_vector.h
+ * Author: root
+ *
+ * Created on October 23, 2015, 2:59 PM
+ */
+
+#ifndef MEMIO_VECTOR_H
+#define	MEMIO_VECTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <p18f46k22.h>
+#include <GenericTypeDefs.h>
+#include <timers.h>
+
+#define	TIMEROFFSET		32000           // timer0 16bit counter value for 1 second to overflow
+#define	PDELAY			28000	// refresh for I/O	
+
+	/* DIO defines */
+#define LOW				(unsigned char)0        // digital output state levels, sink
+#define	HIGH            (unsigned char)1        // digital output state levels, source
+#define	ON				LOW       		//
+#define OFF				HIGH			//
+#define	S_ON            LOW       		// low select/on for chip/led
+#define S_OFF           HIGH			// high deselect/off chip/led
+#define	R_ON            HIGH       		// control relay states, relay is on when output gate is high, uln2803,omron relays need the CPU at 5.5vdc to drive
+#define R_OFF           LOW			// control relay states
+#define R_ALL_OFF       0x00
+#define R_ALL_ON		0xff
+#define NO				LOW
+#define YES				HIGH
+#define IN				HIGH
+#define OUT				LOW
+
+#define	A10				PORTCbits.PORTC0 // RAM/ROM select LOW for ROM starting at address 0
+#define WAIT			LATCbits.LATC1  // memory access delay
+#define	ZDATA			LATD
+#define ZRD				LATBbits.LATB4
+#define ZM1				LATBbits.LATB5
+
+#ifdef DLED_DEBUG
+#ifdef P46K22
+#define DLED0		LATDbits.LATD0
+#define DLED1		LATDbits.LATD1
+#define DLED2		LATDbits.LATD2
+#define DLED3		LATDbits.LATD3
+#define DLED4		LATDbits.LATD4
+#define DLED5		LATDbits.LATD5
+#define DLED6		LATAbits.LATA6
+#define DLED7		LATAbits.LATA7
+#endif
+#else
+#define DLED2			LATCbits.LATC2
+#define DLED7			LATCbits.LATC7
+#endif
+
+#ifdef INTTYPES
+#include <stdint.h>
+#else
+#define INTTYPES
+	/*unsigned types*/
+	typedef unsigned char uint8_t;
+	typedef unsigned int uint16_t;
+	typedef unsigned long uint32_t;
+	typedef unsigned long long uint64_t;
+	/*signed types*/
+	typedef signed char int8_t;
+	typedef signed int int16_t;
+	typedef signed long int32_t;
+	typedef signed long long int64_t;
+#endif
+
+	/*
+	 * the logic is true HIGH for STATE
+	 */
+	struct z80_type { // internal state table
+		uint16_t RUN : 1;
+		uint16_t MREQ : 1;
+		uint16_t IORQ : 1;
+		uint16_t RD : 1;
+		uint16_t WR : 1;
+		uint16_t M1 : 1;
+		uint16_t ISRAM : 1;
+		uint16_t S1 : 1;
+		uint16_t S2 : 1;
+		uint16_t paddr;
+		uint16_t maddr;
+		uint8_t data;
+	};
+
+	extern volatile struct z80_type Z;
+	extern volatile uint8_t z80_ram[256];
+	extern const rom int8_t z80_rom[1024];
+	extern volatile uint8_t data_in2;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MEMIO_VECTOR_H */
+
