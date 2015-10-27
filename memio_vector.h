507ec3551db0e9ec46a052c738d4a189e6f8b627
Fred Brooks <fred@sma2.rain.com>
2015-10-27 16:19:39 -0700

    update

--- memio_vector.h
+++ memio_vector.h
@@ -16,37 +16,38 @@ extern "C" {
 #include <p18f46k22.h>
 #include <GenericTypeDefs.h>
 #include <timers.h>
+#include "ringbufs.h"
 
-#define	TIMEROFFSET		32000           // timer0 16bit counter value for 1 second to overflow
-#define	PDELAY			28000			// refresh for I/O	
+#define	TIMEROFFSET	32000           // timer0 16bit counter value for 1 second to overflow
+#define	PDELAY		28000			// refresh for I/O	
 
 	/* general DIO defines */
-#define LOW				(unsigned char)0        // digital output state levels, sink
+#define LOW		(unsigned char)0        // digital output state levels, sink
 #define	HIGH            (unsigned char)1        // digital output state levels, source
-#define	ON				LOW       		//
-#define OFF				HIGH			//
+#define	ON		LOW       		//
+#define OFF		HIGH			//
 #define	S_ON            LOW       		// low select/on for chip/led
 #define S_OFF           HIGH			// high deselect/off chip/led
 #define	R_ON            HIGH       		// control relay states, relay is on when output gate is high, uln2803,omron relays need the CPU at 5.5vdc to drive
 #define R_OFF           LOW				// control relay states
 #define R_ALL_OFF       0x00
-#define R_ALL_ON		0xff
-#define NO				LOW
-#define YES				HIGH
-#define IN				HIGH
-#define OUT				LOW
+#define R_ALL_ON	0xff
+#define NO		LOW
+#define YES		HIGH
+#define IN		HIGH
+#define OUT		LOW
 
 	/* Z80 pins */
-#define	A10				PORTCbits.RC0 // RAM/ROM select LOW for ROM starting at address 0
-#define WAIT			LATCbits.LATC1  // memory access delay
-#define	ZDATA			LATD
-#define ZRD				PORTBbits.RB4
-#define ZM1				PORTBbits.RB5
-#define ZRFSH 			PORTBbits.RB6
+#define	A10		PORTCbits.RC0 // RAM/ROM select LOW for ROM starting at address 0
+#define WAIT		LATCbits.LATC1  // memory access delay
+#define	ZDATA		LATD
+#define ZRD		PORTBbits.RB4
+#define ZM1		PORTBbits.RB5
+#define ZRFSH 		PORTBbits.RB6
 
 	/* debug pins */
-#define DLED2			LATCbits.LATC2
-#define DLED7			LATCbits.LATC7
+#define DLED2		LATCbits.LATC2
+#define DLED7		LATCbits.LATC7
 
 #ifdef INTTYPES
 #include <stdint.h>
@@ -82,6 +83,17 @@ extern "C" {
 		uint16_t maddr;
 		uint8_t data;
 	};
+	
+	struct spi_link_type { // internal state table
+	uint8_t SPI_LCD : 1;
+	uint8_t SPI_AUX : 1;
+	uint8_t LCD_TIMER : 1;
+	uint8_t LCD_DATA : 1;
+	uint16_t delay;
+	uint8_t config;
+	struct ringBufS_t *tx1b, *tx1a;
+	int32_t int_count;
+};
 
 	extern volatile struct z80_type Z;
 	extern volatile uint8_t z80_ram[256];
