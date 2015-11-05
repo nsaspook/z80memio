1479e3bfbb27b8c970f226b5a79ce5408211babf
Fred Brooks <nsaspook@nsaspook.com>
2015-11-05 14:11:31 -0800

    merge

--- untitled0.c
+++ untitled0.c
@@ -5,6 +5,9 @@
 __sfr __at 0x80 IoPort_data; // send display data text
 __sfr __at 0x81 IoPort_cmd; // send commands to program the display
 __sfr __at 0x01 IoPort_bit; // set or clear bit RC6 on the PIC
+__sfr __at 0x10 IoPort_rngl; // random number generator bits, low byte
+__sfr __at 0x11 IoPort_rngh; // random number generator bits, high byte
+__sfr __at 0x00 IoPort_dummy; // SPI dummy read
 
 // MEMIO SRAM memory space 
 volatile __at(0x0400) unsigned char ram_space;
@@ -16,6 +19,10 @@ static const char __at 0x00d0 z80_text[] = " All work and no play makes Jack a d
 // display functions
 void config_display(void);
 void putc(unsigned char);
+
+volatile static unsigned char __at(0x0400) b;
+volatile static unsigned char __at(0x0401) d;
+char z;
 
 void main(void)
 {
@@ -31,12 +38,18 @@ void main(void)
 
 	// send the text string to the data port
 	while (1) {
+		b = IoPort_rngl;
 		for (i = 0; i < 80; i++) {
-			c = z80_text[i];
+			if (b < 128) {
+				c = z80_text[i];
+			} else {
+				c = z80_text0[i];
+			}
 			if (!c) break;
 			putc(c);
-			a = 0;
-			while (a++ < 1024);
+			a = 2000;
+			if (IoPort_data < 200) a = 0;
+			while (a) a--;
 		}
 	};
 }
